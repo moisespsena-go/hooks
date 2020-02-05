8177ada385d3083484006cd5aca510fffebc10b3
Moises P. Sena <moisespsena@gmail.com>
2020-02-05 17:05:29 -0300

    Update prebuild.go

--- pre-build/prebuild.go
+++ pre-build/prebuild.go
@@ -1,9 +1,9 @@
 package pre_build
 
 import (
-	"github.com/ecletus/hooks"
-	fix_jinzhu_configor_print_to_stdout "github.com/ecletus/hooks/pre-build/fix-jinzhu-configor-print-to-stdout"
-	xbindata_build "github.com/ecletus/hooks/pre-build/xbindata-build"
+	"github.com/moisespsena-go/hooks"
+	fix_jinzhu_configor_print_to_stdout "github.com/moisespsena-go/hooks/pre-build/fix-jinzhu-configor-print-to-stdout"
+	xbindata_build "github.com/moisespsena-go/hooks/pre-build/xbindata-build"
 )
 
 func Hooks() hooks.Jobs {
